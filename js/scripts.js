635f4ce76c2b63c9f4f69e5776f703ca0f8f8492
AnanyevAlex <a1113035@yandex.ru>
2017-12-08 18:15:25 +0500

    123

--- js/scripts.js
+++ js/scripts.js
@@ -2,11 +2,10 @@ var link = document.querySelector(".button-find-hotel");
 var popup = document.querySelector(".main-booking-form");
 var arrdate = popup.querySelector("[name=arrival-date]");
 
- popup.classList.add("hidden-booking-form");
+popup.classList.add("hidden-booking-form");
 
-  link.addEventListener("click", function(evt) {
-    evt.preventDefault ();
-    popup.classList.toggle("hidden-booking-form");
-    arrdate.focus();
-    console.log("Кликнули по кнопке бронирования");
-  });
+link.addEventListener("click", function(evt) {
+  evt.preventDefault ();
+  popup.classList.toggle("hidden-booking-form");
+  arrdate.focus();
+});
